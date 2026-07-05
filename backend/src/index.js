310a2e7bed114a6537147340eae268f191eb0c69
Saurabh Dipte <93472902+saurabhdipte@users.noreply.github.com>
2026-07-04 20:18:02 -0400

    Implement database connection and user/message models; update server initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import { connectDB } from "./lib/db.js"
+import { clerkMiddleware } from "@clerk/express"
+import cors from "cors";
 
 
 const app = express();
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL
 
-app.listen(PORT, () =>  console.log("Server is up and running on port" , PORT))
+
+// middleware
+app.use(express.json());
+app.use(cors({origin:FRONTEND_URL, credentials:true}));
+app.use(clerkMiddleware());
+
+app.listen(PORT, () =>  {
+    connectDB();
+    console.log("Server is up and running on port" , PORT);
+    
+});
